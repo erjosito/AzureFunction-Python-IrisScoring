77f065c4722b24798bf1e6f8582f39d9047ae6da
Jose Moreno <jomore@microsoft.com>
2019-07-12 09:27:41 +0200

    first commit

---
+++ IrisInference/__init__.py
@@ -0,0 +1,75 @@
+# Azure Function for scoring a model trained on the iris dataset
+#
+# July 2019, FastTrack for Azure - jose.moreno@microsoft.com
+#
+# Simple function for loading a model trained on the Iris dataset with joblib and score data supplied in JSON payload
+# The model could have been trained in different ways, for example, using Azure ML.
+#
+# Using joblib to serialize the model because it tends to be more solid than pickle for sklearn models
+
+import logging
+import azure.functions as func
+import json, joblib, os
+import numpy as np
+
+# Importing sklearn not required for scoring
+# from sklearn.svm import SVC
+
+def init(model_filename):
+    print("Loading model...")
+    try:
+        model = joblib.load(model_filename)
+        return model
+    except:
+        return None
+
+# note you can pass in multiple rows for scoring
+def predict(model, raw_data):
+    try:
+        #data = json.loads(raw_data)['data']
+        data = raw_data['data']
+        data = np.array(data)
+        logging.info("Predicting: " + str(data))
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        logging.error('Error when trying to load model')
+        result = str(e)
+        return result
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    # Let's init some variables
+    model_filename = './irismodel.pkl'
+    req_body = req.get_json()
+
+    # Try to get a model
+    model = init(model_filename)
+    if model is None:
+        # We include some troubleshooting info in the response if the model file could not be found
+        response = {'operation_result': 'error - model file {0} not found'.format(model_filename),
+            'data': str(req_body['data']),
+            'cwd': os.getcwd(),
+            'files_in_cwd': str(os.listdir()) }
+        response_string = json.dumps(response)
+        return func.HttpResponse(response_string, status_code=400)
+
+    # With the model, run the prediction
+    logging.info('Starting prediction')
+    score_result = predict(model, req_body)
+    logging.info('Prediction result: ' + str(score_result))
+
+    # Verify we got a list, everything else is considered to be an error
+    if isinstance(score_result, list):
+        response = {'operation_result': 'success', 
+                    'data': str(req_body['data']),
+                    'prediction': str(score_result) }
+        response_string = json.dumps(response)
+        return func.HttpResponse(response_string)
+    else:
+        response = {'operation_result': 'error',
+                    'data': str(req_body['data']),
+                    'prediction': str(score_result),
+                    'prediction data type': str(type(score_result)) }
+        response_string = json.dumps(response)
+        return func.HttpResponse(response_string, status_code=400)
---
+++ README.md
@@ -0,0 +1,56 @@
+# Python Function to score a previously trained ML model
+
+This function loads a previously trained machine learning model (in this particular case using sklearn and Azure ML Service) and scores it with data supplied over POST calls. You can look at it as the hello world of ML scoring.
+
+The Azure ML Service supports deployment of registered models to a variety of target compute platforms, but at this point in time Azure Functions is not one of them. It is fairly easy though training a model, exporting it using joblib (I am using here joblib instead of pickle because it tends to work better for sklearn models), and package it next to the function.
+
+The source code of this function includes an irismodel.pkl file that contains a model previously trained with the iris dataset.
+
+## Quick Deploy to Azure
+
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.svg)](https://azuredeploy.net/)
+
+## Configuration
+
+Please follow these steps:
+
+### To use the provided model
+
+* Deploy this function, and score it sending for example this JSON payload (one example of each class is provided here, so the prediction result should look like `[0, 1, 2]`):
+
+```
+{
+	"data": [
+		[ "5.1", "3.5", "1.4", "0.2" ],
+		["7.0", "3.2", "4.7", "1.4"],
+		["6.3", "3.3", "6.0", "2.5"]
+	]
+}
+```
+
+The answer should look like this:
+
+```
+{
+  "operation_result": "success",
+  "data": "[['5.1', '3.5', '1.4', '0.2'], ['7.0', '3.2', '4.7', '1.4'], ['6.3', '3.3', '6.0', '2.5']]",
+  "prediction": "[0, 1, 2]"
+}
+```
+
+### Change to use with your own model
+
+If you decide to move over the hello world stage, you might want to follow these steps to deploy with your own model:
+
+* Train a ML model as you prefer. You can use for example [this tutorial for training a model with sklearn and the Azure ML Service](https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/machine-learning/service/how-to-train-scikit-learn.md)
+* Create a Python HTTP-triggered Azure Function. You can follow [this tutorial](https://docs.microsoft.com/azure/azure-functions/functions-create-first-function-python)
+* Include the serialized model (.pkl file) in your function directory, modify your code at will
+* Send HTTP Post calls. The exact format of the body will depend on how you trained your model.
+
+## Application settings
+
+No application settings required.
+
+## Running Locally
+
+Visual Studio function app project is included.
